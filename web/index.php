f59b5ee7f6e7540a9352594328ef6f02c19b96b9
Marek Matulka <marek@matulka.net>
2016-08-31 08:55:49 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,12 @@
 <?php
 
-phpinfo();
+try {
+    $pdo = new \PDO(
+        'mysql:host=mysql;dbname=db_name',
+        'user',
+        'pass'
+    );
+    echo "DB connection works!";
+} catch (\Exception $e) {
+    echo "Error: {$e->getMessage()}\n";
+}
